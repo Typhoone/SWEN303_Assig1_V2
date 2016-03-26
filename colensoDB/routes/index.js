942fec344fcff25b41642d30642b103c2ed4f3bf
Gordon Adams <gordonadamsnz@gmail.com>
2016-03-26 17:30:06 +1300

    Nav Bar fix
    Included Jquery to fix the navbar not working
    -_-

--- colensoDB/routes/index.js
+++ colensoDB/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Colenso' });
 });
 
+router.get('/search', function(req, res, next) {
+  res.render('search', { title: 'Colenso Search' });
+});
+
 module.exports = router;

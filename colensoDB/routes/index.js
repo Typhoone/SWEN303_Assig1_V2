5c848d18a9de491cfec896af158b81efd92bf50d
Gordon Adams <gordonadamsnz@gmail.com>
2016-03-27 17:36:57 +1300

    First Xquery
    Xquery returns a result

--- colensoDB/routes/index.js
+++ colensoDB/routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+
+client.execute("OPEN Colenso");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +13,23 @@ router.get('/', function(req, res, next) {
 
 router.get('/search', function(req, res, next) {
   res.render('search', { title: 'Colenso Search' });
+});
+
+router.post('/processXq', function(req, res, next) {
+  var query = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; " + req.body.searchField;
+  var value;
+
+  client.execute(query, function(err, reslt){
+    if(err){
+      value = "No results found \n ERROR: " + err;
+    }else{
+      value = reslt.result;
+    }
+    res.render('results', {title : 'Results', val : value})
+  });
+
+
+
 });
 
 module.exports = router;

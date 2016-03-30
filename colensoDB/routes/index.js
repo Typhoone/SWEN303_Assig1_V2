15905586dba93f9e99c1a37048bd8b7a1130c5eb
Gordon Adams <gordonadamsnz@gmail.com>
2016-03-30 16:51:37 +1300

    Fixed bunk code
    Fixed something that was working, but was very wrong
    ahh the magic of js

--- colensoDB/routes/index.js
+++ colensoDB/routes/index.js
@@ -17,54 +17,68 @@ router.get('/search', function(req, res, next) {
   res.render('search', { title: 'Search' });
 });
 
+function runQuery(query, req, res){
+  var value;
+
+  client.execute(query, function(err, reslt){
+    if(req.body.searchField){
+      if(err || reslt.ok === false || !(reslt.result.length > 0)){
+        value = "No results found ";
+        if(err){
+          value += "<br> ERROR: " + err;
+        }else{
+          err = 'not found';
+        }
+      }else{
+        value = reslt.result.split('\r\n');
+
+      }
+    }
+
+    res.render('results', {title : 'Results', val : value, err : err})
+  });
+}
+
 router.post('/processXq', function(req, res, next) {
   // var query = "XQUERY declare namespace tei= 'http://www.tei-c.org/ns/1.0'; " + req.body.searchField;
   var query = 'XQUERY declare default element namespace "http://www.tei-c.org/ns/1.0"; for $n in ('+ req.body.searchField +')\n' +
                  'return db:path($n)';
-  var value;
+  runQuery(query, req, res);
+});
 
+router.post('/processSimple', function(req, res, next) {
+  // var query = "XQUERY declare namespace tei= 'http://www.tei-c.org/ns/1.0'; " + req.body.searchField;
+  var query = 'XQUERY declare default element namespace "http://www.tei-c.org/ns/1.0"; for $v in .//TEI[. contains text ' + req.body.searchField + '] return db:path($v)';
+  console.log('/********************************************query*******************************\\');
+  console.log(query);
+  runQuery(query, req, res);
+});
 
-
-  router.get('/viewFile', function(req, res, next) {
-    fs.readFile('../Colenso/' + req.query.file, function(err, data){
-      if(err) {
-        throw err;
-      }
-      $ = cheerio.load(data);
-
-      title= $('title').text();
-      author= $('author').text();
-      date= $('date').text();
-      front= $('front');
-      body= $('body');
-
-
-
-
-      res.render('viewFile', { fileName:  req.query.file, filePath: '../Colenso/' + req.query.file, title : title, author : author, date : date, front : front, body : body});
-
-
-    });
-
-  });
-
-  router.get('/viewRaw', function(req, res, next) {
-    res.download('../Colenso/' + req.query.file);
-  });
-
-  // In event of not found
-
-  client.execute(query, function(err, reslt){
-    if(req.body.searchField){
-      if(err){
-        value = "No results found \n ERROR: " + err;
-      }else{
-        value = reslt.result.split('\r\n');
-      }
+router.get('/viewFile', function(req, res, next) {
+  fs.readFile('../Colenso/' + req.query.file, function(err, data){
+    if(err) {
+      throw err;
     }
-    res.render('results', {title : 'Results', val : value})
+    $ = cheerio.load(data);
+
+    title= $('title').text();
+    author= $('author').text();
+    date= $('date').text();
+    front= $('front');
+    body= $('body');
+
+
+
+
+    res.render('viewFile', { fileName:  req.query.file, filePath: '../Colenso/' + req.query.file, title : title, author : author, date : date, front : front, body : body});
+
+
   });
 
+});
+
+router.get('/viewRaw', function(req, res, next) {
+  res.download('../Colenso/' + req.query.file);
 });
 
 module.exports = router;
